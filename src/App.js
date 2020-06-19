d88ac1e0eedab5ca5ef8237789c14ad8e6a97f8f
Baros <baros.andry@gmail.com>
2020-06-19 22:57:46 +0700

    '

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Splash from './pages/Splash'
+import { GetStarted } from './pages'
+// import GetStarted from './pages/GetStarted'
+
+const App = () => {
+  return (
+
+      <GetStarted/>  
+   
+  )
+}
+
+export default App
+
